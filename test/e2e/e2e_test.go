49d4649cfaa8b17abf7b1c1cc641449a40327f4f
Roberto Santalla <roobre@roobre.es>
2021-05-12 16:13:39 +0200

    add e2e tests skeleton and pod creation

---
+++ test/e2e/e2e_test.go
@@ -0,0 +1,270 @@
+// Package e2e_test implements simple integration test against a local cluster, whose config is loaded from the kubeconfig file.
+package e2e_test
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"path"
+	"strings"
+	"testing"
+	"time"
+
+	sdkEvent "github.com/newrelic/infra-integrations-sdk/data/event"
+	"github.com/newrelic/nri-kube-events/pkg/events"
+	"github.com/newrelic/nri-kube-events/test/e2e"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// TestPodCreation checks that events related to pod creation are received.
+func TestPodCreation(t *testing.T) {
+	client, agentMock := initialize(t)
+
+	ns, err := client.CoreV1().Namespaces().Create(context.Background(), &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "e2e-" + strings.ToLower(t.Name()) + "-",
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Logf("could not create %s namespace: %v", ns, err)
+	}
+
+	defer func() {
+		t.Log("Cleaning up test namespace...")
+		_ = client.CoreV1().Namespaces().Delete(context.Background(), ns.Name, metav1.DeleteOptions{})
+	}()
+
+	testpod, err := client.CoreV1().Pods(ns.Name).Create(context.Background(), &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx-e2e",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "nginx",
+					Image: "nginx",
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Errorf("could not create test pod: %v", err)
+		return
+	}
+
+	agentMock.Wait(10*time.Second, 1*time.Minute)
+	for _, event := range []sdkEvent.Event{
+		// All strings are matched in a very relaxed way, using strings.Contains(real, test)
+		{
+			Summary:  "Successfully assigned " + ns.Name + "/" + testpod.Name + " to ",
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Scheduled",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Successfully assigned " + ns.Name + "/" + testpod.Name + " to ",
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+		{
+			Summary:  "Pulling image \"" + testpod.Spec.Containers[0].Image + "\"",
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Pulling",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Pulling image \"" + testpod.Spec.Containers[0].Image + "\"",
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+		{
+			Summary:  "Successfully pulled image \"" + testpod.Spec.Containers[0].Image + "\" in ",
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Pulled",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Successfully pulled image \"" + testpod.Spec.Containers[0].Image + "\" in ",
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+		{
+			Summary:  "Created container " + testpod.Spec.Containers[0].Name,
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Created",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Created container " + testpod.Spec.Containers[0].Name,
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+		{
+			Summary:  "Started container " + testpod.Spec.Containers[0].Name,
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Started",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Started container " + testpod.Spec.Containers[0].Name,
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+	} {
+		if agentMock.Has(&event) {
+			continue
+		}
+
+		e := json.NewEncoder(os.Stderr)
+		t.Log("Expected:")
+		_ = e.Encode(event)
+		t.Fatalf("Event was not captured")
+	}
+}
+
+func TestPodDeletion(t *testing.T) {
+	client, agentMock := initialize(t)
+
+	ns, err := client.CoreV1().Namespaces().Create(context.Background(), &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "e2e-" + strings.ToLower(t.Name()) + "-",
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Logf("could not create %s namespace: %v", ns, err)
+	}
+
+	defer func() {
+		t.Log("Cleaning up test namespace...")
+		_ = client.CoreV1().Namespaces().Delete(context.Background(), ns.Name, metav1.DeleteOptions{})
+	}()
+
+	testpod, err := client.CoreV1().Pods(ns.Name).Create(context.Background(), &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx-e2e-killable",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "nginx",
+					Image: "nginx",
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Errorf("could not create test pod: %v", err)
+		return
+	}
+
+	time.Sleep(7 * time.Second)
+
+	err = client.CoreV1().Pods(ns.Name).Delete(context.Background(), testpod.Name, metav1.DeleteOptions{})
+	if err != nil {
+		t.Errorf("could not create test pod: %v", err)
+		return
+	}
+
+	agentMock.Wait(15*time.Second, 1*time.Minute)
+	for _, event := range []sdkEvent.Event{
+		{
+			Summary:  "Stopping container " + testpod.Spec.Containers[0].Name,
+			Category: "kubernetes",
+			Attributes: map[string]interface{}{
+				"event.metadata.name":             testpod.Name + ".",
+				"event.metadata.namespace":        ns.Name,
+				"event.reason":                    "Killing",
+				"clusterName":                     "",
+				"event.involvedObject.apiVersion": "",
+				"event.involvedObject.kind":       "Pod",
+				"event.involvedObject.name":       testpod.Name,
+				"event.message":                   "Stopping container " + testpod.Spec.Containers[0].Name,
+				"event.type":                      "Normal",
+				"verb":                            "ADDED",
+			},
+		},
+	} {
+		if agentMock.Has(&event) {
+			continue
+		}
+
+		e := json.NewEncoder(os.Stderr)
+		t.Log("Expected:")
+		e.Encode(event) // nolint:errcheck
+		t.Fatalf("Event was not captured")
+	}
+}
+
+// initialize returns a kubernets client and a mocked agent sink ready to receive events
+func initialize(t *testing.T) (*kubernetes.Clientset, *e2e.MockedAgentSink) {
+	t.Helper()
+
+	conf, err := restConfig()
+	if err != nil {
+		t.Fatalf("could not build kubernetes config: %v", err)
+	}
+
+	client, err := kubernetes.NewForConfig(conf)
+	if err != nil {
+		t.Fatalf("could not build kubernetes client: %v", err)
+	}
+
+	sharedInformers := informers.NewSharedInformerFactory(client, time.Duration(0))
+	eventsInformer := sharedInformers.Core().V1().Events().Informer()
+	sharedInformers.Start(nil)
+	sharedInformers.WaitForCacheSync(nil)
+	for _, obj := range eventsInformer.GetStore().List() {
+		_ = eventsInformer.GetStore().Delete(obj)
+	}
+
+	agentMock := e2e.NewMockedAgentSink()
+	router := events.NewRouter(eventsInformer, map[string]events.Sink{"mock": agentMock})
+	go router.Run(nil)
+
+	return client, agentMock
+}
+
+// restConfig attempts to build a k8s config from the environment, or the default kubeconfig path
+func restConfig() (*rest.Config, error) {
+	config, err := rest.InClusterConfig()
+	if err == nil {
+		return config, err
+	}
+
+	config, err = clientcmd.BuildConfigFromFlags("", path.Join(os.ExpandEnv("$HOME"), ".kube", "config"))
+	if err == nil {
+		return config, err
+	}
+
+	return nil, err
+}
---
+++ test/e2e/mocked_agent.go
@@ -0,0 +1,174 @@
+package e2e
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"sync"
+	"time"
+
+	sdkEvent "github.com/newrelic/infra-integrations-sdk/data/event"
+	"github.com/newrelic/nri-kube-events/pkg/events"
+	"github.com/newrelic/nri-kube-events/pkg/sinks"
+	log "github.com/sirupsen/logrus"
+)
+
+// Must be in sync with unexported name in pkg/sinks/new_relic_infra.go:32.
+const newRelicInfraSinkID = "newRelicInfra"
+
+// MockedAgentSink is an instrumented infra-agent sink for testing e2e reception and processing.
+type MockedAgentSink struct {
+	agentSink         events.Sink
+	httpServer        *httptest.Server
+	eventReceivedChan chan struct{}
+	receivedEvents    []sdkEvent.Event
+	mtx               *sync.RWMutex
+}
+
+// NewMockedAgentSink returns an instrumented infra-agent sink for testing.
+func NewMockedAgentSink() *MockedAgentSink {
+	mockedAgentSink := &MockedAgentSink{
+		mtx:               &sync.RWMutex{},
+		eventReceivedChan: make(chan struct{}, 8),
+	}
+	mockedAgentSink.httpServer = httptest.NewServer(mockedAgentSink)
+
+	agentSinkConfig := sinks.SinkConfig{
+		Name: newRelicInfraSinkID,
+		Config: map[string]string{
+			"clusterName":   "integrationTest",
+			"agentEndpoint": "http://" + mockedAgentSink.httpServer.Listener.Addr().String(),
+		},
+	}
+
+	createdSinks, err := sinks.CreateSinks([]sinks.SinkConfig{agentSinkConfig})
+	if err != nil {
+		log.Fatalf("error creating infra sink: %v", err)
+	}
+
+	agentSink, ok := createdSinks[newRelicInfraSinkID]
+	if !ok {
+		log.Fatal("could not retrieve agent infra sink from map")
+	}
+
+	mockedAgentSink.agentSink = agentSink
+
+	return mockedAgentSink
+}
+
+// HandleEvent sends a notification to the event received channel and then forwards it to the underlying sink.
+func (mas *MockedAgentSink) HandleEvent(kubeEvent events.KubeEvent) error {
+	mas.eventReceivedChan <- struct{}{}
+	return mas.agentSink.HandleEvent(kubeEvent)
+}
+
+// ServeHTTP handles a request that would be for the infra-agent and stores the unmarshalled event.
+func (mas *MockedAgentSink) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	mas.mtx.Lock()
+	defer mas.mtx.Unlock()
+
+	var ev struct {
+		Data []struct {
+			Events []sdkEvent.Event `json:"events"`
+		} `json:"data"`
+	}
+
+	defer r.Body.Close() // nolint:errcheck
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &ev)
+	if err != nil {
+		log.Fatalf("error getting request body")
+	}
+
+	mas.receivedEvents = append(mas.receivedEvents, ev.Data[0].Events...)
+
+	rw.WriteHeader(http.StatusNoContent) // The the agent does
+}
+
+// Has relaxedly checks whether the mocked agent has received an event.
+func (mas *MockedAgentSink) Has(testEvent *sdkEvent.Event) bool {
+	mas.mtx.RLock()
+	defer mas.mtx.RUnlock()
+
+	for i := range mas.receivedEvents {
+		receivedEvent := &mas.receivedEvents[i]
+
+		if isEventSubset(receivedEvent, testEvent) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Wait blocks until betweenEvents time has passed since the last received event, or up to max time has passed since the call.
+// Returns false if we had to exhaust max.
+func (mas *MockedAgentSink) Wait(betweenEvents, max time.Duration) bool {
+	eventTimer := time.NewTimer(betweenEvents)
+	maxTimer := time.NewTimer(max)
+
+	for {
+		select {
+		// Reset betweenEvents timer whenever an event is received
+		case <-mas.eventReceivedChan:
+			if !eventTimer.Stop() {
+				<-eventTimer.C
+			}
+			eventTimer.Reset(betweenEvents)
+
+		// Return false if max timeout is reached
+		case <-maxTimer.C:
+			return false
+
+		// Return true when small timeout is reached
+		case <-eventTimer.C:
+			return true
+		}
+	}
+}
+
+// isEventSubset checks whether the new event is a subset of the old event.
+func isEventSubset(old, new *sdkEvent.Event) bool {
+	if old == new {
+		return true
+	}
+
+	if !strings.Contains(old.Category, new.Category) ||
+		!strings.Contains(old.Summary, new.Summary) {
+		return false
+	}
+
+	// Check new map is a subset of old map
+	for nk, nv := range new.Attributes {
+		// Check the old event contains all keys of the new one
+		ov, found := old.Attributes[nk]
+		if !found {
+			return false
+		}
+
+		// Ensure types are equal.
+		if reflect.TypeOf(ov) != reflect.TypeOf(nv) {
+			return false
+		}
+
+		// If both are strings, check the old contains the new (partial matching).
+		// Otherwise, just check for equality
+		switch nvs := nv.(type) {
+		case string:
+			ovs := ov.(string)
+			if !strings.Contains(ovs, nvs) {
+				return false
+			}
+		default:
+			if !reflect.DeepEqual(ov, nv) {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
